a804ea31198c2afc1dadcf5aad750509e39c3fff
Alexander Shevelev <al.e.shevelev@gmail.com>
2022-08-13 21:26:42 +0300

    The key-value storage has been added

---
+++ pomodoro_timer/lib/core/storage/storage_keys.dart
@@ -0,0 +1,11 @@
+class StorageKeys {
+  static const pomodoroDuration = "POMODORO_DURATION";
+  static const shortBreakDuration = "SHORT_BREAK_DURATION";
+  static const longBreakDuration = "LONG_BREAK_DURATION";
+
+  static const pomodoroInSeries = "POMODORO_IN_SERIES";
+  static const pomodoroTotal = "POMODORO_TOTAL";
+
+  static const autostartBreak = "AUTOSTART_BREAK";
+  static const autostartPomodoro = "AUTOSTART_POMODORO";
+}
